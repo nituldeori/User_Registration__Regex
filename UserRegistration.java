ae49cb6638e4ab1f85fbe37321794d49f298547b
unknown <nituldeori63@gmail.com>
2020-11-23 00:07:50 +0530

    Validated Last Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,14 +4,14 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 	public static void main(String[] args) {
 	    Scanner sc=new Scanner(System.in);
-	    System.out.println("Enter the First Name: ");
-	    String FirstName=sc.nextLine();
+	    System.out.println("Enter the Last Name: ");
+	    String LastName=sc.nextLine();
 	    String regex="[A-Z]{1}[a-z]{2,}";
-	    if(Pattern.matches(regex, FirstName)) {
-		    System.out.println("Valid First Name");
+	    if(Pattern.matches(regex, LastName)) {
+		    System.out.println("Valid Last Name");
 	    }
 	    else
-		    System.out.println("Invalid First Name");
+		    System.out.println("Invalid Last Name");
 	    sc.close();
 	    }
 	
